0b8c685692f31871a0ce5f3e04d725da0cd2b408
Tim Menzies <timm@ieee.org>
2022-02-13 20:09:09 -0500

    save

--- src/sublime.py
+++ src/sublime.py
@@ -52,11 +52,12 @@ OPTIONS:
 :: [repo](https://github.com/timm/sublime)
 :: [view source](https://github.com/timm/sublime/blob/main/docs/pdf)
 
-<a href="https://zenodo.org/badge/latestdoi/452530453"><img src="https://zenodo.org/badge/452530453.svg"></a>
-<img src="https://img.shields.io/badge/purpose-se,ai-informational?style=flat&logo=hyper&logoColor=white&color=blueviolet">
-<img src="https://img.shields.io/badge/language-python3-informational?style=flat&logo=lua&logoColor=white&color=orange">
-<img src="https://img.shields.io/badge/platform-osx,linux-informational?style=flat&logo=linux&logoColor=white&color=pink">
-<a href="https://github.com/timm/sublime/actions/workflows/main.yml"><img src="https://github.com/timm/sublime/actions/workflows/main.yml/badge.svg"></a>
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.5912461.svg)](https://doi.org/10.5281/zenodo.5912461)
+![](https://img.shields.io/badge/purpose-se--ai-blueviolet)
+![](https://img.shields.io/badge/language-python3-orange)
+![](https://img.shields.io/badge/platform-osx,linux-pink)
+<a href=https://github.com/timm/sublime/actions/workflows/main.yml><img
+src=https://github.com/timm/sublime/actions/workflows/main.yml/badge.svg></a>
 
 ## Algorithm
 
