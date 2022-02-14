558fcf7c63a20f52fd61683f340068a42f940b14
Tim Menzies <timm@ieee.org>
2022-02-13 19:18:05 -0500

    save

--- src/sublime.py
+++ src/sublime.py
@@ -52,7 +52,13 @@ OPTIONS:
 :: [repo](https://github.com/timm/sublime)
 :: [view source](https://github.com/timm/sublime/blob/main/docs/pdf)
 
-[![sublime-all](https://github.com/timm/sublime/actions/workflows/main.yml/badge.svg)](https://github.com/timm/sublime/actions/workflows/main.yml)
+
+<a href="https://github.com/timm/sublime/actions/workflows/main.yml"><img src="https://github.com/timm/sublime/actions/workflows/main.yml/badge.svg"></a>
+<img src="https://img.shields.io/badge/purpose-se,ai-informational?style=flat&logo=hyper&logoColor=white&color=blueviolet">
+<img src="https://img.shields.io/badge/language-python3-informational?style=flat&logo=lua&logoColor=white&color=orange">
+<img src="https://img.shields.io/badge/platform-osx,linux-informational?style=flat&logo=linux&logoColor=white&color=pink">
+<a href="https://zenodo.org/badge/latestdoi/452530453"><img src="https://zenodo.org/badge/452530453.svg"></a>
+
 
 ## Algorithm
 
